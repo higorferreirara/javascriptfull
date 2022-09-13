1f2efb9f6ac185b0cb5c1d5e2b63ae77eb56f892
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2022-09-13 19:37:00 -0300

    Objectos

---
+++ aula17/index.js
@@ -0,0 +1,29 @@
+   //const raiz = function (n) {
+   //     return n** 0.5
+   //};
+   //ou
+
+   const raiz = n => n ** 0.5
+
+   console.log(raiz(81))
+   
+   
+   
+    /*function soma(x, y) {
+        const resultado = x + y
+        return resultado
+    }
+    
+    console.log(soma(2, 7))
+    
+    
+    function saudacao(nome) {
+        return `Bom dia! ${nome}!`;
+    }
+
+    const variavel = saudacao('Higor')
+    console.log(variavel)
+    saudacao('Higor')
+    saudacao('Asafe')
+    saudacao('Miguel')
+    */
---
+++ aula18/objetos.js
@@ -0,0 +1,22 @@
+function criaPessoa (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    }
+}
+
+const pessoa1 = criaPessoa('Asafe', 'Ethan', 2)
+console.log(pessoa1)
+
+
+
+
+/*const pessoa1 = {
+    nome: 'Higor',
+    sobrenome: 'Ferreira',
+    idade: 25
+}
+console.log(pessoa1.nome)
+console.log(pessoa1.sobrenome)
+*/
