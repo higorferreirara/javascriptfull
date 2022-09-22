8e107eb9cb2106e715bbe66912643e68b73be2f6
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2022-09-21 22:41:16 -0300

    Update index.js

--- aula32/index.js
+++ aula32/index.js
@@ -18,5 +18,5 @@ console.log(resto);
 //                    0          1          2
 //                 0  1  2    0  1  2    0  1  2
 const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
-const [,[, , seis ] = numeros
+const [,[, , seis] ] = numeros
 console.log(seis);
