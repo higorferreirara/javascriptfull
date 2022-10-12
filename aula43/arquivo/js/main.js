aa6ce08aed39270c6325f5bc5fd6c9c0c5de19f7
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2022-10-11 22:37:37 -0300

    relogio

--- aula43/arquivo/js/main.js
+++ aula43/arquivo/js/main.js
@@ -1,18 +1,41 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-br', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0
+let timer;
 
-iniciar.addEventListener('click', function(event) {
-    relogio.innerHTML = setInterval(mostrarHora())
+function iniciarRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciarRelogio();
 });
 
-pausar.addEventListener('click', function(event) {
-    alert('cliquei no pausar');
+pausar.addEventListener('click', function (event) {
+    clearInterval(timer);
+    relogio.classList.add('pausado');
 });
 
-zerar.addEventListener('click', function(event) {
-    alert('cliquei no zerar');
+zerar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0
 });
 
 function mostrarHora() {
