858e22e1470d3080c853b441b423a7907150a100
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2023-01-27 23:36:00 -0300

    iniciando calculadora com JS

---
+++ calculadora/arquivo/js/main.js
@@ -0,0 +1,12 @@
+function criaCalculadora() {
+    return {
+
+        inicia() {
+            alert('oi, iniciei!')
+        },
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
