96b6d46cc4c94cc9f82f78b6bcda08b50427c48c
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2023-03-02 22:20:23 -0300

    Metodos para objetos

---
+++ aula52/metodosObj.js
@@ -0,0 +1,10 @@
+const produto = {nome: 'produto', preco: 1.8};
+const caneca = {
+    ...produto,
+    material: 'porcelana'
+};
+
+caneca.nome = 'Outro nome';
+caneca.preco = 2.5;
+console.log(produto)
+console.log(caneca);
